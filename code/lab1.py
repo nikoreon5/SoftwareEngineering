4eebe482927f9af03c9c6b2071ba1f11501f3739
= <=>
2025-10-28 10:51:28 +0500

    Test

--- code/lab1.py
+++
@@ -1,7 +0,0 @@
-class Car:
-    def __init__(self, make, model): # конструктор класса
-        self.make = make # обращение к атрибуту класса
-        self.model = model # обращение к атрибуту класса
-
-my_car = Car('Toyota', 'Corolla') # вызываем конструктор класса, передаем параметры make и model
-print(my_car.make, my_car.model)
